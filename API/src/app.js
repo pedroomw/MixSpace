d4f4e1e7507c6fa1485a238433182551478666c7
pedroomw <malowicki.pedro@gmail.com>
2026-06-07 22:18:42 -0300

    Agregar CORS para aceptar cross-requests

--- API/src/app.js
+++ API/src/app.js
@@ -1,11 +1,18 @@
 import 'dotenv/config'
 import express from "express"
 import filesRouter from './routes/files-controller.js'
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.get('/', (req, res) => {
   res.json({ mensaje: 'API funcionando' });
