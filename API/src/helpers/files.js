5330a76144ea0d822e0904750c53c6dbb071d929
Android Studio <devandroid@ort>
2026-07-03 12:02:33 -0300

    Todo funcionando. Falta lo de agregar filename automaticamente, tal vez mediante un middleware

--- API/src/helpers/files.js
+++
@@ -1,9 +0,0 @@
-import path from "path"
-import getFormattedDate from './dates.js'
-
-const setRandomFilename = (originalName) => {
-    let filename = `${getFormattedDate()}-${Math.random().toString(36).slice(2,8)}${path.extname(originalName)}`
-    return filename
-}
-
-export default setRandomFilename
