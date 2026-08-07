c0375ca83754dd85b35eaa58c2762da6243bf076
IGNACIO ARIEL KOHN <48793643@ort>
2026-08-07 12:12:59 -0300

    Hacer el signup??

--- API/src/repositories/auth-repository.js
+++ API/src/repositories/auth-repository.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('SUPABASE_URL and SUPABASE_ANON_KEY must be set in environment')
@@ -13,7 +13,9 @@ class AuthRepository {
     signUp = async (email, password) => {
         const { data, error } = await supabaseAuth.auth.signUp({ email, password })
         if (error) throw error
-        return data
+        const { data: profileData, error: profileError } = await supabaseAuth.from("Users").insert({ id: data.user.id, email: email, password: password }).select()
+        if(profileError) throw profileError
+        return profileData
     }
 
     signIn = async (email, password) => {
